11dabae3d08eefe93a426d219779c46cf5514887
JM1415 <jmacopiado15@gmail.com>
2022-03-18 14:55:24 +0800

    ivadd updat

--- src/app/invadd/invadd.component.ts
+++ src/app/invadd/invadd.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormGroup, FormControl, NgForm,  } from '@angular/forms';
+import { FormGroup, FormControl, NgForm  } from '@angular/forms';
 import { Product } from '../models/Product';
 import { ProductService } from '../services/product.service'
 
