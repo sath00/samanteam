977b2872b08c4962dd2616ff51797bb96bc46759
sanicodes <jgsanico1@up.edu.ph>
2022-03-21 20:21:45 +0800

    removed unecessary documentation

--- src/app/invadd/invadd.component.ts
+++ src/app/invadd/invadd.component.ts
@@ -23,42 +23,4 @@ export class InvaddComponent implements OnInit{
       this.productService.addProduct(form.value.ProductName,form.value.ProductDisc,form.value.ProductPrice,form.value.ProductCat,form.value.ProductImg, form.value.ProductQuant);
       form.resetForm();
   }
-
-  /*THIS DOESNT WORK IGNORE IGNORE IGNORE IGNORE IGNORE
-  form: FormGroup; //form of type FormGroup
-  product: Product; //product of type Product defined in models/Product.ts
-  imageData: string; //path of image data will be referenced as a string
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.form = new FormGroup ({
-      name: new FormControl(null),
-      description: new FormControl(null),
-      price: new FormControl(null),
-      category: new FormControl(null),
-      image: new FormControl(null),
-      quantity: new FormControl(null)
-    });
-  }
-
-  onFileSelect(event: Event) {
-    const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
-    const allowedMimeTypes = ["image/png", "image/jpeg", "image/jpg"];
-    if (file && allowedMimeTypes.includes(file.type)) { //if choesn file is of the correct type
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imageData = reader.result as string; //data turns into string represting image
-      }
-      reader.readAsDataURL(file); //reads file and converts it to image
-    }
-  }
-
-  onSubmit() {
-    console.log('submitted product');
-    this.form.reset();      //resets form
-    this.imageData = '';  //clears image data
-  }
-  */
 }
