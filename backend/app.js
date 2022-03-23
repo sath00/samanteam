56fbe3d548b1bab6a10337b8bde81981197c8f05
sanicodes <jgsanico1@up.edu.ph>
2022-03-23 10:41:19 +0800

    updated API for availability

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,10 @@ const app = express();
 
 
 //function that I used to connect to our mongodb
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(result => {
-    console.log("Connected to database")
-}).catch(err => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log("Connected to database")
+    }).catch(err => console.log(err));
 
 
 //used the body parser for parsing posts requests data this is later used in /add-items
@@ -27,55 +27,17 @@ app.use(bodyparser.json());
 //cors headers
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     next();
 })
 
 //api for deleting items
-app.delete('/api/remove-item/:id', (req, res) => {  
-    Product.deleteOne({_id:mongoose.Types.ObjectId(req.params.id)}).then(result => {
-        if(result.deletedCount>0){
+app.delete('/api/remove-item/:id', (req, res) => {
+    Product.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) }).then(result => {
+        if (result.deletedCount > 0) {
             res.status(200).json({
                 message: 'Product was succesfully deleted!'
-            });
-        }else{
-            res.status(200).json({
-                message: 'Product was not found!'
-            });
-        }
-    })
-})
-
-
-//api for adding items 
-app.post('/api/add-items',(req, res)=>{
-    const product = new Product({
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        category:req.body.category,
-        image:req.body.image,
-        availability:req.body.availability
-    });
-    product.save().then((result) =>{
-        res.status(200).json({
-        message:'Product added successfully!'});
-    }).catch((err) =>{
-        res.status(400).json({
-            error:err._message
-        })
-    })
-})
-
-//this is the api for marking items as sold out or available
-
-app.post('/api/:id/availability', (req,res)=>{
-    Product.updateOne({_id:mongoose.Types.ObjectId(req.params.id)},{availability:req.body.availability})
-    .then(result=>{
-        if (result.modifiedCount > 0) {
-            res.status(200).json({
-                message: 'Product was succesfully Updated!'
             });
         } else {
             res.status(200).json({
@@ -86,17 +48,55 @@ app.post('/api/:id/availability', (req,res)=>{
 })
 
 
-
-//this is the api for getting the items in the database
-app.get('/api/items',(req, res) => {
-    Product.find()
-    .then((result)=>{
-        res.status(200).json(result);
+//api for adding items 
+app.post('/api/add-items', (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: req.body.image,
+        availability: req.body.availability
+    });
+    product.save().then((result) => {
+        res.status(200).json({
+            message: 'Product added successfully!'
+        });
     }).catch((err) => {
         res.status(400).json({
             error: err._message
-        }) 
+        })
     })
+})
+
+//this is the api for marking items as sold out or available
+
+app.post('/api/item/availability', (req, res) => {
+    Product.updateOne({ _id:req.body._id }, { availability: req.body.availability })
+        .then(result => {
+            if (result.modifiedCount > 0) {
+                res.status(200).json({
+                    message: 'Product was succesfully Updated!'
+                });
+            } else {
+                res.status(200).json({
+                    message: 'Product was not found!'
+                });
+            }
+        })
+})
+
+
+//this is the api for getting the items in the database
+app.get('/api/items', (req, res) => {
+    Product.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(400).json({
+                error: err._message
+            })
+        })
 })
 
 
