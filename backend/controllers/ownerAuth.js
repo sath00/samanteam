bdfc6fbba75c5116c298e2000fd11f05122f9bf1
sanicodes <jgsanico1@up.edu.ph>
2022-06-08 17:15:53 +0800

    backend fixes and deployment ready

--- backend/controllers/ownerAuth.js
+++ backend/controllers/ownerAuth.js
@@ -1,5 +1,5 @@
 const Owner = require('../models/owner');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 exports.editCredentials = async (req, res) => {
