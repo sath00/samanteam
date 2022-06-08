e25ecfd3b9614ce78f2b9b25df36a468ba5c976d
sanicodes <jgsanico1@up.edu.ph>
2022-06-08 18:00:28 +0800

    backend fixes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 //imported the http module server creation
 const http = require('http');
 //setting up the port and creating the server
-const port = 3000;
-const app = require('./app');
+const port = process.env.PORT || 3000;
+const app = require('./appRoutes');
 const server = http.createServer(app);
 server.listen(port);
 console.log("Server Listening on port " + port);
